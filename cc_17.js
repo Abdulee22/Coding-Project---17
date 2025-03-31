c85a5fb655586a8df26ba80649f4f54803e5d491
Abdulhakim <Abdulthe383@gmail.com>
2025-03-30 22:57:36 -0400

    Task 2 : Created Sales Rep Class

--- cc_17.js
+++ cc_17.js
@@ -5,13 +5,31 @@ constructor(name, email) {
     this.email = email;// Customer email 
     this.purchaseHistory = [];} // Initialize array 
     
-    addPurchase(amount) {
-    this.purchaseHistory.push(amount);} // Purchase amount 
-    getTotalSpent() {
-    return this.purchaseHistory.reduce((total, amount) => total + amount, 0);}}// Total amount calculation 
+addPurchase(amount) {
+ this.purchaseHistory.push(amount);} // Purchase amount 
+getTotalSpent() {
+return this.purchaseHistory.reduce((total, amount) => total + amount, 0);}}// Total amount calculation 
     
-    const customer1 = new Customer("Goku", "Kakarot@Gmail.com");
-    customer1.addPurchase(200); // 200$ Purchase 
-    customer1.addPurchase(350); // 350$ Purchase
-    console.log(`Customer: ${customer1.name}, Total Spent: $${customer1.getTotalSpent()}`); // Customer situation 
+const customer1 = new Customer("Goku", "Kakarot@Gmail.com");
+customer1.addPurchase(200); // 200$ Purchase 
+customer1.addPurchase(350); // 350$ Purchase
+console.log(`Customer: ${customer1.name}, Total Spent: $${customer1.getTotalSpent()}`); // Customer situation 
+
+    // Task 2: Create a SalesRep Class //
+class SalesRep {
+constructor(name) {
+    this.name = name; // Store rep 
+    this.clients = [];} // Initialize array 
+        
+addClient(customer) {
+this.clients.push(customer);} // Adding customer 
+        
+getClientTotal(name) {
+const client = this.clients.find(client => client.name === name);
+return client ? client.getTotalSpent() : 0;}} // finds client by name and returns total spent
+    
+const salesRep = new SalesRep("Krillin");
+salesRep.addClient(customer1);
+console.log(`SalesRep: ${salesRep.name}, Total Spent by ${customer1.name}: $${salesRep.getClientTotal("Goku")}`); // Customer situation 
+    
     
